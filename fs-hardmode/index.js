6374a94da394be9970d71b930a65afca69955876
Tom Stuart <tom@tomstuart.co.uk>
2020-08-31 12:28:12 +0200

    Add FSHM page

---
+++ fs-hardmode/index.js
@@ -0,0 +1,28 @@
+var points = [[52.560069, 13.309797],
+              [53.011165, 11.640379],
+              [53.558045, 9.998084],
+              [52.458608, 5.524312],
+              [52.320055, 4.779994]
+             ];
+var next = [[52.320055, 4.779994], [50.848447, 4.350446], [50.899377, 4.457018]];
+var map = L.map('map-container').setView(points[0], 5);
+
+L.tileLayer('https://c.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    maxZoom: 18,
+    tileSize: 512,
+    zoomOffset: -1
+}).addTo(map);
+
+L.polyline(points, {
+    color: 'black'
+}).addTo(map);
+
+L.polyline(next, {
+    color: 'black',
+    dashArray: '9'
+}).addTo(map);
+
+L.marker([52.560069, 13.309797]).bindPopup("<b>Start</b>").openPopup().addTo(map);
+L.marker([53.011165, 11.640379]).bindPopup("<b>Leg 1 (<a href='https://www.twitch.tv/videos/723267146'>Video</a>)</b>").openPopup().addTo(map);
+L.marker([52.320055, 4.779994]).bindPopup("<b>Leg 2 (<a href='https://www.twitch.tv/videos/725063939'>Video</a>)</b>").openPopup().addTo(map);
