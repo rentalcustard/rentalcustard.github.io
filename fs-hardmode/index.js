9062f016be22005a883601251ba5844c03c7a38f
Tom Stuart <tom@tomstuart.co.uk>
2020-09-09 07:25:34 +0200

    Add marker for Luxembourg leg

--- fs-hardmode/index.js
+++ fs-hardmode/index.js
@@ -8,7 +8,7 @@ var points = [[52.560069, 13.309797],
               [50.899377, 4.457018],
               [49.618016, 6.188661]
              ];
-var next = [[50.899377, 4.457018], [49.618016, 6.188661], [48.859821, 2.336384], [49.024639, 2.524955]];
+var next = [[49.618016, 6.188661], [48.859821, 2.336384], [49.024639, 2.524955]];
 var map = L.map('map-container').setView(points[0], 5);
 
 L.tileLayer('https://a.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -31,3 +31,4 @@ L.marker([52.560069, 13.309797]).bindPopup("<b>Start</b>").openPopup().addTo(map
 L.marker([53.011165, 11.640379]).bindPopup("<b>Leg 1 (<a href='https://www.twitch.tv/videos/723267146'>Video</a>)</b>").openPopup().addTo(map);
 L.marker([52.320055, 4.779994]).bindPopup("<b>Leg 2 (<a href='https://www.twitch.tv/videos/725063939'>Video</a>)</b>").openPopup().addTo(map);
 L.marker([50.899377, 4.457018]).bindPopup("<b>Leg 3 (<a href='https://www.twitch.tv/videos/729190127'>Video</a>)</b>").openPopup().addTo(map);
+L.marker([49.618016, 6.188661]).bindPopup("<b>Leg 3 (<a href='https://www.twitch.tv/videos/735177595'>Video</a>)</b>").openPopup().addTo(map);
