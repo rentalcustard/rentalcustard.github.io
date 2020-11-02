64f750e80a03e0040c3c6444f7d6424be4bbe770
Tom Stuart <tom@tomstuart.co.uk>
2020-11-02 10:06:56 +0100

    Update with latest progress

--- fs-hardmode/index.js
+++ fs-hardmode/index.js
@@ -8,9 +8,11 @@ var points = [[52.560069, 13.309797],
               [50.899377, 4.457018],
               [49.618016, 6.188661],
               [48.859821, 2.336384],
-              [48.949165, 2.427425]
+              [48.949165, 2.427425],
+              [48.713009, 0.006501],
+              [49.208854, -2.184938]
              ];
-var next = [[48.949165, 2.427425], [48.634824, -1.510737], [49.206924, -2.206593]];
+var next = [[49.208854, -2.184938], [49.435274, -2.592150], [51.324050, 0.027023]];
 var map = L.map('map-container').setView(points[0], 5);
 
 L.tileLayer('https://a.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -33,4 +35,6 @@ L.marker([52.560069, 13.309797]).bindPopup("<b>Start</b>").openPopup().addTo(map
 L.marker([53.011165, 11.640379]).bindPopup("<b>Leg 1 (<a href='https://www.twitch.tv/videos/723267146'>Video</a>)</b>").openPopup().addTo(map);
 L.marker([52.320055, 4.779994]).bindPopup("<b>Leg 2 (<a href='https://www.twitch.tv/videos/725063939'>Video</a>)</b>").openPopup().addTo(map);
 L.marker([50.899377, 4.457018]).bindPopup("<b>Leg 3 (<a href='https://www.twitch.tv/videos/729190127'>Video</a>)</b>").openPopup().addTo(map);
-L.marker([49.618016, 6.188661]).bindPopup("<b>Leg 3 (<a href='https://www.twitch.tv/videos/735177595'>Video</a>)</b>").openPopup().addTo(map);
+L.marker([49.618016, 6.188661]).bindPopup("<b>Leg 4 (<a href='https://www.twitch.tv/videos/735177595'>Video</a>)</b>").openPopup().addTo(map);
+L.marker([48.949165, 2.427425]).bindPopup("<b>Leg 5 (<a href='https://www.twitch.tv/videos/743868603'>Video</a>)</b>").openPopup().addTo(map);
+L.marker([49.208854, -2.184938]).bindPopup("<b>Leg 7 (<a href='https://www.twitch.tv/videos/787649666'>Video</a>)</b>").openPopup().addTo(map);
